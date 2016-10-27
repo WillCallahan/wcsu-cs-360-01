cd309050d1f9373498ed87435e211815bad8e70c
William Callahan <willcallahan94@att.net>
2016-10-26 23:45:41 -0400

    Change to project structure to separate segments of the application

--- src/main/java/edu/wcsu/cs360/battleship/domain/dmo/local/Blindboard.java
+++
@@ -1,9 +0,0 @@
-package edu.wcsu.cs360.battleship.domain.dmo.local;
-
-public class Blindboard {
-	private boolean[][] brd = new boolean[5][5];
-
-	public void mark(int x, int y) {
-		brd[x][y] = true;
-	}
-}
